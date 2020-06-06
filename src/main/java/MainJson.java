30d27a53c2dc498e4fee7bad0343ca00afbb9aa9
brenobeirigo <brenobeirigo@gmail.com>
2020-06-06 12:30:17 +0200

    Add sortWaitingUsersByClass and streamline rebalance settings

--- src/main/java/MainJson.java
+++ src/main/java/MainJson.java
@@ -12,6 +12,11 @@ import simulation.Simulation;
 import simulation.SimulationFCFS;
 import simulation.Solution;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainJson {
 
@@ -20,30 +25,49 @@ public class MainJson {
 
 
         InstanceConfig instanceSettings = InstanceConfig.getInstance();
-        int levelInfo = Simulation.ALL_INFO;
+
+        int infoLevel = Simulation.ROUND_INFO;
 
         // Vary test case parameters
-        for (int timeHorizon : instanceSettings.getTimeHorizonArray()) {
-            for (int maxRequestsIteration : instanceSettings.getMaxRequestsIterationArray()) {
-                for (int timeWindow : instanceSettings.getTimeWindowArray()) {
-                    for (int vehicleMaxCapacity : instanceSettings.getVehicleMaxCapacityArray()) {
-                        for (int initialFleet : instanceSettings.getInitialFleetArray()) {
-                            for (boolean isAllowedToHire : instanceSettings.getAllowVehicleHiringArray()) {
-                                for (boolean isAllowedToLowerServiceLevel : instanceSettings.getAllowServiceDeteriorationArray()) {
+        for (boolean sortWaitingUsersByClass : instanceSettings.getSortWaitingUsersByClass()) {
+            for (int timeHorizon : instanceSettings.getTimeHorizonArray()) {
+                for (int maxRequestsIteration : instanceSettings.getMaxRequestsIterationArray()) {
+                    for (int timeWindow : instanceSettings.getTimeWindowArray()) {
+                        for (int vehicleMaxCapacity : instanceSettings.getVehicleMaxCapacityArray()) {
+                            for (int initialFleet : instanceSettings.getInitialFleetArray()) {
+                                for (boolean isAllowedToHire : instanceSettings.getAllowVehicleHiringArray()) {
+                                    for (boolean isAllowedToLowerServiceLevel : instanceSettings.getAllowServiceDeteriorationArray()) {
 
-                                    // If can hire than service level have to be lowered
-                                    if (isAllowedToHire != isAllowedToLowerServiceLevel) continue;
+                                        // If can hire than service level have to be lowered
+                                        if (isAllowedToHire != isAllowedToLowerServiceLevel) continue;
 
-                                    for (int contractDuration : instanceSettings.getContractDurationArray()) {
+                                        for (int contractDuration : instanceSettings.getContractDurationArray()) {
 
-                                        for (CustomerBaseConfig customerBaseSettings : instanceSettings.getCustomerBaseSettingsArray()) {
-                                            // Update global class configuration to run current test case
-                                            Config.getInstance().qosDic = customerBaseSettings.qosDic;
+                                            for (CustomerBaseConfig customerBaseSettings : instanceSettings.getCustomerBaseSettingsArray()) {
+                                                // Update global class configuration to run current test case
+                                                Config.getInstance().qosDic = customerBaseSettings.qosDic;
 
-                                            for (boolean allowRebalancing : instanceSettings.getAllowRebalancingArray()) {
+                                                for (boolean allowRebalancing : instanceSettings.getAllowRebalancingArray()) {
 
-                                                if (allowRebalancing) {
-                                                    for (Rebalance rebalanceSettings : instanceSettings.getListRebalanceSettings()) {
+                                                    Instant before = Instant.now();
+                                                    List<Rebalance> allRebalancingSettings = new ArrayList<>();
+
+                                                    if (!allowRebalancing) {
+                                                        Rebalance rebalanceSettings = new Rebalance(
+                                                                false,
+                                                                false,
+                                                                false,
+                                                                false,
+                                                                "Rebalancing",
+                                                                false,
+                                                                false
+                                                        );
+
+                                                        allRebalancingSettings.add(rebalanceSettings);
+                                                    } else {
+                                                        allRebalancingSettings = instanceSettings.getListRebalanceSettings();
+                                                    }
+                                                    for (Rebalance rebalanceSettings : allRebalancingSettings) {
 
                                                         // Create FCFS simulation
                                                         Simulation fcfs = new SimulationFCFS(
@@ -57,12 +81,13 @@ public class MainJson {
                                                                 contractDuration,
                                                                 isAllowedToHire,
                                                                 isAllowedToLowerServiceLevel,
+                                                                sortWaitingUsersByClass,
                                                                 customerBaseSettings.serviceRateLabel,
                                                                 customerBaseSettings.customerSegmentationLabel,
                                                                 rebalanceSettings);
 
                                                         // Run simulation
-                                                        fcfs.run(levelInfo);
+                                                        fcfs.run(infoLevel);
 
                                                         // Reset classes for next iteration
                                                         Dao.getInstance().resetRecords();
@@ -74,48 +99,14 @@ public class MainJson {
                                                         Simulation.reset();
                                                         Solution.reset();
                                                     }
-                                                } else {
 
-                                                    Rebalance rebalanceSettings = new Rebalance(
-                                                            false,
-                                                            false,
-                                                            false,
-                                                            false,
-                                                            "Rebalancing",
-                                                            false,
-                                                            false
-                                                    );
-                                                    // Create FCFS simulation
-                                                    Simulation fcfs = new SimulationFCFS(
-                                                            instanceSettings.getInstanceName(),
-                                                            initialFleet,
-                                                            vehicleMaxCapacity,
-                                                            maxRequestsIteration,
-                                                            timeWindow,
-                                                            timeHorizon,
-                                                            allowRebalancing,
-                                                            contractDuration,
-                                                            isAllowedToHire,
-                                                            isAllowedToLowerServiceLevel,
-                                                            customerBaseSettings.serviceRateLabel,
-                                                            customerBaseSettings.customerSegmentationLabel,
-                                                            rebalanceSettings);
+                                                    Instant after = Instant.now();
+                                                    Duration duration = Duration.between(before, after);
+                                                    System.out.println("Duration:" + duration.toMillis());
 
-                                                    // Run simulation
-                                                    fcfs.run(levelInfo);
-
-                                                    // Reset classes for next iteration
-                                                    Dao.getInstance().resetRecords();
-                                                    User.reset();
-                                                    Vehicle.reset();
-                                                    Node.reset();
-                                                    NodeMiddle.reset();
-                                                    Visit.reset();
-                                                    Simulation.reset();
-                                                    Solution.reset();
                                                 }
+                                                Config.reset();
                                             }
-                                            Config.reset();
                                         }
                                     }
                                 }
