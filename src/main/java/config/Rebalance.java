e2a85075f2846ef84e3a10eb5509016603ccadbc
brenobeirigo <brenobeirigo@gmail.com>
2020-06-07 09:45:37 +0200

    optimal rebalancing from Alonso-Mora

--- src/main/java/config/Rebalance.java
+++ src/main/java/config/Rebalance.java
@@ -1,13 +1,13 @@
 package config;
 
 import dao.Dao;
+import gurobi.*;
+import model.User;
 import model.Vehicle;
 import model.node.Node;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Rebalance {
 
@@ -58,6 +58,138 @@ public class Rebalance {
         str += useUrgentKey ? "_UR" : "";
 
         return str;
+    }
+
+
+    public void rebalanceOptimal(Set<Vehicle> idleVehicles, List<User> roundRejectedUsers) {
+
+        try {
+
+            if (roundRejectedUsers == null || roundRejectedUsers.isEmpty())
+                return;
+
+            //Extract origins from rejected requests
+            List<Node> targets = roundRejectedUsers.stream().map(User::getNodePk).collect(Collectors.toList());
+
+            /*System.out.println("Targets: " + targets.size());
+            System.out.println("Vehicles: " + idleVehicles.size());
+            System.out.println("TARGETS=" + nTargets + " -- VEHICLES:" + nIdleVehicles);*/
+
+            // Model
+            GRBEnv env = new GRBEnv();
+            GRBModel model = new GRBModel(env);
+            model.set(GRB.StringAttr.ModelName, "assignment_vehicles_to_rejected");
+
+            // Assignment variables: x[v][n] == 1 if vehicle v is assigned to node n.
+            GRBVar[][] x = new GRBVar[idleVehicles.size()][targets.size()];
+
+            int v=0, n = 0;
+
+            // Vehicle conservation ////////////////////////////////////////////////////////////////////////////////////
+            GRBLinExpr constrMaxNumberOfRelocations = new GRBLinExpr();
+
+            for (Vehicle vehicle : idleVehicles) {
+                n = 0;
+
+                GRBLinExpr constrVehicleConservation = new GRBLinExpr();
+
+                for (Node target : targets) {
+
+                    double distance_vehicle_target = Dao.getInstance().getDistKm(vehicle.getLastVisitedNode(), target);
+                    String label = v + "_" + vehicle.getId() + "_" + n + "_" + target.getTripId();
+                    x[v][n] = model.addVar(0, 1, distance_vehicle_target, GRB.CONTINUOUS, label);
+                    constrMaxNumberOfRelocations.addTerm(1, x[v][n]);
+                    constrVehicleConservation.addTerm(1, x[v][n]);
+                    n++;
+                }
+
+                // A vehicle can visit at most a target
+                model.addConstr(constrVehicleConservation, GRB.LESS_EQUAL, 1, "assignment_" + vehicle);
+                v++;
+            }
+
+            // Request conservation ////////////////////////////////////////////////////////////////////////////////////
+            n = 0;
+            for (Node target : targets) {
+                GRBLinExpr constrTargetConservation = new GRBLinExpr();
+                for (v = 0; v < idleVehicles.size(); v++) {
+                    constrTargetConservation.addTerm(1, x[v][n]);
+                }
+
+                // A target can be visited by at most one vehicle
+                model.addConstr(constrTargetConservation, GRB.LESS_EQUAL, 1, "node_" + n + "_" + target.getTripId());
+                n++;
+            }
+
+            // N. of rebalancing trips does not surpass number of vehicles or targets
+            model.addConstr(constrMaxNumberOfRelocations, GRB.EQUAL, Math.min(targets.size(), idleVehicles.size()), "n_rebalancing_trips");
+
+            // The objective is to minimize the total pay costs
+            model.set(GRB.IntAttr.ModelSense, GRB.MINIMIZE);
+
+            // Optimize
+            model.optimize();
+            int status = model.get(GRB.IntAttr.Status);
+            if (status == GRB.Status.UNBOUNDED) {
+                System.out.println("The model cannot be solved because it is unbounded");
+                return;
+            }
+            if (status == GRB.Status.OPTIMAL) {
+                System.out.println("The optimal objective is " + model.get(GRB.DoubleAttr.ObjVal));
+
+                System.out.println("Optimal rebalancing:");
+
+                v = 0;
+                for (Vehicle vehicle : idleVehicles) {
+                    n = 0;
+
+                    for (Node target : targets) {
+
+                        int result = (int) x[v][n].get(GRB.DoubleAttr.X);
+
+                        if (result > 0) {
+
+                            // Make vehicle move to target
+                            if (vehicle.getLastVisitedNode().getNetworkId() != target.getNetworkId()) {
+                                vehicle.rebalanceTo(target);
+                            }
+                            //else {
+                                //System.out.println("-->>>>>" + label + " " + result + names.get(v).get(n) + " = " + distance + "distance2=" + distance2);
+                            //}
+                        }
+                        n++;
+                    }
+                    v++;
+                }
+
+
+                return;
+            }
+            if (status != GRB.Status.INF_OR_UNBD &&
+                    status != GRB.Status.INFEASIBLE) {
+                System.out.println("Optimization was stopped with status " + status);
+                return;
+            }
+
+            // Compute IIS
+            System.out.println("The model is infeasible; computing IIS");
+            model.computeIIS();
+            System.out.println("\nThe following constraint(s) "
+                    + "cannot be satisfied:");
+            for (GRBConstr c : model.getConstrs()) {
+                if (c.get(GRB.IntAttr.IISConstr) == 1) {
+                    System.out.println(c.get(GRB.StringAttr.ConstrName));
+                }
+            }
+
+            // Dispose of model and environment
+            model.dispose();
+            env.dispose();
+
+        } catch (GRBException e) {
+            System.out.println("Error code: " + e.getErrorCode() + ". " +
+                    e.getMessage());
+        }
     }
 
     /**
