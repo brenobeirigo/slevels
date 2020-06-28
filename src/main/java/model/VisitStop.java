f1b3bd0376d37ea198216d81fa4297ab7f30f182
brenobeirigo <brenobeirigo@gmail.com>
2020-06-28 22:04:43 +0200

    Visit stop (dummy for assignment)

---
+++ src/main/java/model/VisitStop.java
@@ -0,0 +1,23 @@
+package model;
+
+import model.node.Node;
+
+import java.util.stream.Collectors;
+
+public class VisitStop extends Visit {
+
+
+    public VisitStop(Vehicle v) {
+        this.vehicle = v;
+        this.delay = 0;
+        this.idle = 0;
+    }
+
+    public String toString() {
+        return String.format("# Vehicle %s stopped at %s.", this.vehicle, this.vehicle, this.vehicle.getLastVisitedNode());
+    }
+
+    public String getVarId(){
+        return this.getVehicle().getLastVisitedNode().toString();
+    }
+}
