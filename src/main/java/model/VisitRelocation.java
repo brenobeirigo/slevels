36c5182120e32b8aba0ef26a563e58092c4d7242
brenobeirigo <brenobeirigo@gmail.com>
2020-06-28 22:05:51 +0200

    relocation delay is zero

--- src/main/java/model/VisitRelocation.java
+++ src/main/java/model/VisitRelocation.java
@@ -3,6 +3,8 @@ package model;
 import dao.Dao;
 import model.node.Node;
 
+import java.util.stream.Collectors;
+
 public class VisitRelocation extends Visit {
 
     protected Node targetNode;
@@ -11,6 +13,7 @@ public class VisitRelocation extends Visit {
     public VisitRelocation(Node target, Vehicle v) {
         this.targetNode = target;
         this.vehicle = v;
+        this.delay = 0;
 
         // Arrival at target node (latest time at vehicle current node + distance to target)
         this.targetArrival = v.getDepartureCurrent() + Dao.getInstance().getDistSec(v.getLastVisitedNode(), target);
@@ -26,5 +29,9 @@ public class VisitRelocation extends Visit {
 
     public Node getTargetNode() {
         return this.targetNode;
+    }
+
+    public String getVarId(){
+        return this.getTargetNode().toString();
     }
 }
