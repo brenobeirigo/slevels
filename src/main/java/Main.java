5a637d77b9b794260f434c76c769f987c1562910
brenobeirigo <brenobeirigo@gmail.com>
2020-06-30 06:07:34 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,9 @@ import model.Vehicle;
 import model.Visit;
 import model.node.Node;
 import model.node.NodeMiddle;
-import simulation.Simulation;
-import simulation.SimulationFCFS;
-import simulation.SimulationRTV;
-import simulation.Solution;
+import simulation.*;
+
+import java.lang.reflect.Array;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -39,7 +38,7 @@ public class Main {
 
             // Instances
             jsonConfigFilePath = jsonConfig.get("instance_file_path").toString();
-            System.out.println("# Executing configuration at \""+jsonConfigFilePath+"\"...");
+            System.out.println("# Executing configuration at \"" + jsonConfigFilePath + "\"...");
 
             // Round information level (no information, round summary, all information)
             String infoLevelLabel = jsonConfig.get("info_level").toString();
@@ -47,28 +46,27 @@ public class Main {
 
             //TODO read infoHandling from file
             infoHandling.put(Simulation.SAVE_VEHICLE_ROUND_GEOJSON, false);
-            infoHandling.put(Simulation.SAVE_REQUEST_INFO_CSV , true);
-            infoHandling.put(Simulation.SAVE_ROUND_INFO_CSV , true);
+            infoHandling.put(Simulation.SAVE_REQUEST_INFO_CSV, true);
+            infoHandling.put(Simulation.SAVE_ROUND_INFO_CSV, true);
 
             // Print info in console
-            infoHandling.put(Simulation.SHOW_ALL_VEHICLE_JOURNEYS , false);
-            infoHandling.put(Simulation.SHOW_ROUND_FLEET_STATUS , true);
-            infoHandling.put(Simulation.SHOW_ROUND_INFO , true);
-        }
-        catch (Exception e){
-            System.out.println("Cannot load configuration: "+ e);
+            infoHandling.put(Simulation.SHOW_ALL_VEHICLE_JOURNEYS, false);
+            infoHandling.put(Simulation.SHOW_ROUND_FLEET_STATUS, true);
+            infoHandling.put(Simulation.SHOW_ROUND_INFO, true);
+        } catch (Exception e) {
+            System.out.println("Cannot load configuration: " + e);
             System.out.println("Loading default instance (show round summary)...");
             // Json with instance
             jsonConfigFilePath = "C:\\Users\\LocalAdmin\\IdeaProjects\\slevels\\src\\main\\resources\\default_instance.json";
 
             infoHandling.put(Simulation.SAVE_VEHICLE_ROUND_GEOJSON, true);
-            infoHandling.put(Simulation.SAVE_REQUEST_INFO_CSV , true);
-            infoHandling.put(Simulation.SAVE_ROUND_INFO_CSV , true);
+            infoHandling.put(Simulation.SAVE_REQUEST_INFO_CSV, true);
+            infoHandling.put(Simulation.SAVE_ROUND_INFO_CSV, true);
 
             // Print info in console
-            infoHandling.put(Simulation.SHOW_ALL_VEHICLE_JOURNEYS , true);
-            infoHandling.put(Simulation.SHOW_ROUND_FLEET_STATUS , true);
-            infoHandling.put(Simulation.SHOW_ROUND_INFO , true);
+            infoHandling.put(Simulation.SHOW_ALL_VEHICLE_JOURNEYS, true);
+            infoHandling.put(Simulation.SHOW_ROUND_FLEET_STATUS, true);
+            infoHandling.put(Simulation.SHOW_ROUND_INFO, true);
         }
 
         InstanceConfig instanceSettings = InstanceConfig.getInstance(jsonConfigFilePath);
@@ -94,8 +92,9 @@ public class Main {
 
                                                 for (boolean allowRebalancing : instanceSettings.getAllowRebalancingArray()) {
 
-                                                    Instant before = Instant.now();
                                                     List<Rebalance> allRebalancingSettings = new ArrayList<>();
+                                                    List<Matching> allMatchingSettings = new ArrayList<>();
+
 
                                                     if (!allowRebalancing) {
                                                         Rebalance rebalanceSettings = new Rebalance(
@@ -114,59 +113,54 @@ public class Main {
                                                     }
                                                     for (Rebalance rebalanceSettings : allRebalancingSettings) {
 
-                                                        Simulation rtv = new SimulationRTV(
-                                                                instanceSettings.getInstanceName(),
-                                                                initialFleet,
-                                                                vehicleMaxCapacity,
-                                                                maxRequestsIteration,
-                                                                timeWindow,
-                                                                timeHorizon,
-                                                                allowRebalancing,
-                                                                contractDuration,
-                                                                isAllowedToHire,
+                                                        allMatchingSettings.add(new Matching(
+                                                                Matching.METHOD_OPTIMAL,
                                                                 isAllowedToLowerServiceLevel,
-                                                                sortWaitingUsersByClass,
-                                                                customerBaseSettings.serviceRateLabel,
-                                                                customerBaseSettings.customerSegmentationLabel,
-                                                                rebalanceSettings);
+                                                                customerBaseSettings,
+                                                                contractDuration,
+                                                                rebalanceSettings,
+                                                                isAllowedToHire));
 
-                                                        rtv.run(infoHandling);
+                                                        for (Matching matchingSettings : allMatchingSettings) {
 
-//                                                        // Create FCFS simulation
-//                                                        Simulation fcfs = new SimulationFCFS(
-//                                                                instanceSettings.getInstanceName(),
-//                                                                initialFleet,
-//                                                                vehicleMaxCapacity,
-//                                                                maxRequestsIteration,
-//                                                                timeWindow,
-//                                                                timeHorizon,
-//                                                                allowRebalancing,
-//                                                                contractDuration,
-//                                                                isAllowedToHire,
-//                                                                isAllowedToLowerServiceLevel,
-//                                                                sortWaitingUsersByClass,
-//                                                                customerBaseSettings.serviceRateLabel,
-//                                                                customerBaseSettings.customerSegmentationLabel,
-//                                                                rebalanceSettings);
-//
-//                                                        // Run simulation
-//                                                        fcfs.run(infoHandling);
+                                                            Instant before = Instant.now();
 
-                                                        // Reset classes for next iteration
-                                                        Dao.getInstance().resetRecords();
-                                                        User.reset();
-                                                        Vehicle.reset();
-                                                        Node.reset();
-                                                        NodeMiddle.reset();
-                                                        Visit.reset();
-                                                        Simulation.reset();
-                                                        Solution.reset();
+                                                            // Create FCFS simulation
+                                                            Simulation simulation = new SimulationFCFS(
+                                                                    instanceSettings.getInstanceName(),
+                                                                    initialFleet,
+                                                                    vehicleMaxCapacity,
+                                                                    maxRequestsIteration,
+                                                                    timeWindow,
+                                                                    timeHorizon,
+                                                                    allowRebalancing,
+                                                                    contractDuration,
+                                                                    isAllowedToHire,
+                                                                    isAllowedToLowerServiceLevel,
+                                                                    sortWaitingUsersByClass,
+                                                                    customerBaseSettings.serviceRateLabel,
+                                                                    customerBaseSettings.customerSegmentationLabel,
+                                                                    rebalanceSettings,
+                                                                    matchingSettings);
+
+                                                            // Run simulation
+                                                            simulation.run(infoHandling);
+
+                                                            // Reset classes for next iteration
+                                                            Dao.getInstance().resetRecords();
+                                                            User.reset();
+                                                            Vehicle.reset();
+                                                            Node.reset();
+                                                            NodeMiddle.reset();
+                                                            Visit.reset();
+                                                            Simulation.reset();
+                                                            Solution.reset();
+
+                                                            Instant after = Instant.now();
+                                                            Duration duration = Duration.between(before, after);
+                                                            System.out.println("Duration:" + duration.toMillis());
+                                                        }
                                                     }
-
-                                                    Instant after = Instant.now();
-                                                    Duration duration = Duration.between(before, after);
-                                                    System.out.println("Duration:" + duration.toMillis());
-
                                                 }
                                                 Config.reset();
                                             }
