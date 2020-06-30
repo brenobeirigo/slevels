22abd8144a25382a362669399771f68d6897af43
brenobeirigo <brenobeirigo@gmail.com>
2020-06-30 06:02:12 +0200

    Create ResultAssignment.java

---
+++ src/main/java/simulation/ResultAssignment.java
@@ -0,0 +1,169 @@
+package simulation;
+
+import model.User;
+import model.Vehicle;
+import model.Visit;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ResultAssignment {
+
+    // Set of requests scheduled to vehicles
+    private Set<User> requestsOK;
+
+    // Unassigned requests
+    protected Set<User> requestsUnassigned;
+
+    // Set of users that were displaced from their current rides (is in unassgigned)
+    protected Set<User> requestsDisplaced;
+
+    // Set of vehicles assigned to visits
+    private Set<Vehicle> vehiclesOK;
+
+    // Set of vehicles that interrupted routes and parked
+    protected Set<Vehicle> vehiclesDisrupted;
+
+    // New vehicle is added in list
+    protected Set<Vehicle> vehiclesHired;
+
+    // Users who were picked up by hired vehicle
+    protected List<User> roundPrivateRides;
+
+    // Set of visits chosen in round
+    private Set<Visit> visitsOK;
+
+    public ResultAssignment() {
+
+        // Set of requests scheduled to vehicles
+        requestsOK = new HashSet<>();
+        requestsDisplaced = new HashSet<>();
+        requestsUnassigned = new HashSet<>();
+
+        // Set of vehicles assigned to visits
+        vehiclesOK = new HashSet<>();
+        vehiclesHired = new HashSet<>();
+        roundPrivateRides = new ArrayList<>();
+
+        // Set of vehicles that interrupted routes and parked
+        vehiclesDisrupted = new HashSet<>();
+
+        // Set of visits chosen in round
+        visitsOK = new HashSet<>();
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // PRINTS //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void printRoundResult() {
+
+        System.out.println(String.format("\n\n# Assigned vehicles (%d): %s", vehiclesOK.size(), vehiclesOK));
+        System.out.println(String.format("# Unassigned users (%d): %s", requestsUnassigned.size(), requestsUnassigned));
+        System.out.println(String.format("# Displaced users (%d): %s", requestsDisplaced.size(), requestsDisplaced));
+        System.out.println(String.format("# Vehicles disrupted (%d) = %s", vehiclesDisrupted.size(), vehiclesDisrupted));
+        for (Vehicle vehicle : vehiclesDisrupted) {
+            System.out.println("#### Disrupted = " + vehicle.getVisit());
+        }
+    }
+
+    public void addHiredVehicle(Vehicle vehicle){
+        this.vehiclesHired.add(vehicle);
+    }
+
+    public void addVisit(Visit visit) {
+
+        // Update requests, vehicles, and visitsOK solution
+        this.requestsOK.addAll(visit.getRequests());
+        this.vehiclesOK.add(visit.getVehicle());
+        this.visitsOK.add(visit);
+
+
+        // Remove vehicle and users matched from graph
+        for (User user : visit.getRequests()) {
+
+            // If user changes visit
+            if (user.getCurrentVisit() != null && visit != user.getCurrentVisit()) {
+
+                // Visit of vehicle formerly carrying request will be changed
+                this.vehiclesDisrupted.add(user.getCurrentVisit().getVehicle());
+            }
+        }
+
+        System.out.println(String.format("Setting up %s - User: %s", visit, visit.getUserInfo()));
+        printCurrentStatus();
+    }
+
+    private void printCurrentStatus() {
+        System.out.println(String.format("# Requests (%d): %s", this.requestsOK.size(), this.requestsOK));
+        System.out.println(String.format("# Vehicles (%d): %s", this.vehiclesOK.size(), this.vehiclesOK));
+        System.out.println(String.format("# Disrupted (%d): %s", this.vehiclesDisrupted.size(), this.vehiclesDisrupted));
+        System.out.println(String.format("# Visits: %d", this.visitsOK.size()));
+    }
+
+    private void unassignedUsersCannotBeServiced(){
+        // All vehicles that lost requests rebalance to closest node (middle or next target)
+        for (Vehicle vehicleDisrupted : this.vehiclesDisrupted) {
+
+            assert vehicleDisrupted.getVisit().getPassengers().isEmpty() : "Interrupted vehicle had passenger!" + vehicleDisrupted.getVisit().getUserInfo() + " - Visit:" + vehicleDisrupted.getVisit();
+            assert disruptedUsersAreServicedByDifferentVehicles(vehicleDisrupted) : "Disrupted users (unmatched) are not inserted in different vehicles.";
+            /*for (User u : requestsFormerlyServicedByDisruptedVehicle) {
+                assert u.getCurrentVisit() != null && u.getCurrentVisit() != vehicleDisrupted.getVisit() : String.format("User %s in vehicle %s is still associated with the vehicle", u, vehicleDisrupted);
+            }*/
+
+            /*for (User u : requestsFormerlyServicedByDisruptedVehicle) {
+                assert u.getCurrentVisit() != null && u.getCurrentVisit() != vehicleDisrupted.getVisit() : String.format("User %s in vehicle %s is still associated with the vehicle", u, vehicleDisrupted);
+            }*/
+        }
+    }
+
+    public Set<User> getRequestsOK() {
+        return requestsOK;
+    }
+
+    public void setRequestsOK(Set<User> requestsOK) {
+        this.requestsOK = requestsOK;
+    }
+
+    public Set<Vehicle> getVehiclesOK() {
+        return vehiclesOK;
+    }
+
+    public void setVehiclesOK(Set<Vehicle> vehiclesOK) {
+        this.vehiclesOK = vehiclesOK;
+    }
+
+    public Set<Visit> getVisitsOK() {
+        return visitsOK;
+    }
+
+    public void setVisitsOK(Set<Visit> visitsOK) {
+        this.visitsOK = visitsOK;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // ASSERTIONS //////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private boolean disruptedUsersAreServicedByDifferentVehicles(Vehicle vehicleDisrupted) {
+
+        Set<User> requestsFormerlyServicedByDisruptedVehicle = vehicleDisrupted.getVisit().getRequests();
+
+        Set<User> requestsServicedByDifferentVehicles = requestsFormerlyServicedByDisruptedVehicle.stream()
+                .filter(user -> user.getCurrentVisit().getVehicle() != vehicleDisrupted)
+                .collect(Collectors.toSet());
+
+        // Are all requests serviced in another visits?
+        if (requestsServicedByDifferentVehicles.size() != requestsFormerlyServicedByDisruptedVehicle.size()) {
+            requestsFormerlyServicedByDisruptedVehicle.removeAll(requestsServicedByDifferentVehicles);
+            System.out.println(String.format(" - Users %s from vehicle %s were left unmatched", requestsFormerlyServicedByDisruptedVehicle, vehicleDisrupted));
+            return false;
+        }
+
+        return true;
+    }
+}
