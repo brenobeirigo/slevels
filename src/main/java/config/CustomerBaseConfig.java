2ad6f52586d03fd6f29d9dd6f59f12e8eb2ed70f
brenobeirigo <brenobeirigo@gmail.com>
2020-07-03 16:04:36 +0200

    External Qos

--- src/main/java/config/CustomerBaseConfig.java
+++ src/main/java/config/CustomerBaseConfig.java
@@ -7,9 +7,9 @@ public class CustomerBaseConfig {
     public String serviceRateLabel;
     public String customerSegmentationLabel;
 
-    public Map<String, Config.Qos> qosDic;
+    public Map<String, Qos> qosDic;
 
-    public CustomerBaseConfig(String serviceRateLabel, String customerSegmentationLabel, Map<String, Config.Qos> qosDic) {
+    public CustomerBaseConfig(String serviceRateLabel, String customerSegmentationLabel, Map<String, Qos> qosDic) {
         this.serviceRateLabel = serviceRateLabel;
         this.customerSegmentationLabel = customerSegmentationLabel;
         this.qosDic = qosDic;
