dd988675fa3fb3dbc8b47700b92b68ccb6b5d67c
brenobeirigo <brenobeirigo@gmail.com>
2020-07-06 08:15:44 +0200

    process round results (sort assign users, prints)

--- src/main/java/simulation/ResultAssignment.java
+++ src/main/java/simulation/ResultAssignment.java
@@ -1,11 +1,11 @@
 package simulation;
 
+import config.Config;
 import config.Qos;
 import model.User;
 import model.Vehicle;
 import model.Visit;
 
-import java.awt.datatransfer.SystemFlavorMap;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -14,37 +14,34 @@ public class ResultAssignment {
     public Set<User> requestsServicedLevelAchieved;
     public Map<Qos, Integer> unmetServiceLevelClass;
     public Map<Qos, Integer> totalServiceLevelClass;
-    // Set of requests scheduled to vehicles
-    private Set<User> requestsOK;
-
+    public Set<User> requestsServicedLevelNotAchieved;
     // Unassigned requests
     protected Set<User> requestsUnassigned;
-
     // Set of users that were displaced from their current rides (is in unassgigned)
     protected Set<User> requestsDisplaced;
-
-    // Set of vehicles assigned to visits
-    private Set<Vehicle> vehiclesOK;
-
     // Set of vehicles that interrupted routes and parked
     protected Set<Vehicle> vehiclesDisrupted;
-
     // New vehicle is added in list
     protected Set<Vehicle> vehiclesHired;
-
     // Users who were picked up by hired vehicle
     protected List<User> roundPrivateRides;
-
+    // Result refers to current simulation time
+    private int currentTime;
+    // Set of requests scheduled to vehicles
+    private Set<User> requestsOK;
+    // Set of vehicles assigned to visits
+    private Set<Vehicle> vehiclesOK;
     // Set of visits chosen in round
     private Set<Visit> visitsOK;
 
-    public ResultAssignment() {
-
+    public ResultAssignment(int currentTime) {
+        this.currentTime = currentTime;
         // Set of requests scheduled to vehicles
         requestsOK = new HashSet<>();
         requestsDisplaced = new HashSet<>();
         requestsUnassigned = new HashSet<>();
         requestsServicedLevelAchieved = new HashSet<>();
+        requestsServicedLevelNotAchieved = new HashSet<>();
         unmetServiceLevelClass = new HashMap<>();
         totalServiceLevelClass = new HashMap<>();
 
@@ -65,20 +62,68 @@ public class ResultAssignment {
     // PRINTS //////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public void printRoundResult() {
+    public Map<Qos, List<User>> getClassUnassignedMap() {
 
+        Map<Qos, List<User>> classUnassigned = new HashMap<>();
+
+        for (Qos qos : Config.getInstance().qosDic.values()) {
+            classUnassigned.put(qos, new ArrayList<>());
+        }
+
+        for (User request : this.requestsUnassigned) {
+            classUnassigned.get(request.qos).add(request);
+        }
+
+        return classUnassigned;
+    }
+
+    public Map<Qos, List<User>> getClassSecondTierMap() {
+
+        Map<Qos, List<User>> classSecondTier = new HashMap<>();
+
+        for (Qos qos : Config.getInstance().qosDic.values()) {
+            classSecondTier.put(qos, new ArrayList<>());
+        }
+
+        for (User request : this.requestsServicedLevelNotAchieved) {
+            classSecondTier.get(request.qos).add(request);
+        }
+
+        return classSecondTier;
+    }
+
+    public void printRoundResult() {
+        System.out.println("######## Round current time: " + this.currentTime);
         System.out.println(String.format("\n\n# Assigned vehicles  (%d)  = %s", vehiclesOK.size(), vehiclesOK));
         System.out.println(String.format("# Unassigned users   (%d)  = %s", requestsUnassigned.size(), requestsUnassigned));
         System.out.println(String.format("# Displaced users    (%d)  = %s", requestsDisplaced.size(), requestsDisplaced));
+        System.out.println(String.format("# Class service quality    = %s", overallServiceLevelDistribution()));
         System.out.println(String.format("# Vehicles disrupted (%d)  = %s", vehiclesDisrupted.size(), vehiclesDisrupted));
-        System.out.println("# Class service quality    = " + totalServiceLevelClass.keySet().stream().map(qos -> String.format("%s = %d/%d (%.2f)", qos.id, unmetServiceLevelClass.get(qos), totalServiceLevelClass.get(qos), servicedFirstTier(qos))).collect(Collectors.joining(" - ")));
         for (Vehicle vehicle : vehiclesDisrupted) {
             System.out.println("#### Disrupted = " + vehicle.getVisit());
         }
     }
 
+    private String overallServiceLevelDistribution() {
+        Map<Qos, List<User>> classUnassignedMap = getClassUnassignedMap();
+        Map<Qos, List<User>> class2ndTierMap = getClassSecondTierMap();
+        return totalServiceLevelClass.keySet().stream()
+                .map(qos -> String.format(
+                        "[%s] sl violation = %d/%d (%.1f * %d) (1st-tier = %.2f) - 2nd tier = %s, unassigned: %s",
+                        qos.id,
+                        unmetServiceLevelClass.get(qos),
+                        (int) Math.ceil(qos.serviceRate * totalServiceLevelClass.get(qos)),
+                        qos.serviceRate,
+                        totalServiceLevelClass.get(qos),
+                        servicedFirstTier(qos),
+                        class2ndTierMap.get(qos).size(),
+                        classUnassignedMap.get(qos).size())
+                )
+                .collect(Collectors.joining(" - "));
+    }
+
     double servicedFirstTier(Qos qos) {
-        return (double)(totalServiceLevelClass.get(qos) - unmetServiceLevelClass.get(qos)) / totalServiceLevelClass.get(qos);
+        return (double) (totalServiceLevelClass.get(qos) - unmetServiceLevelClass.get(qos)) / totalServiceLevelClass.get(qos);
     }
 
     public void addHiredVehicle(Vehicle vehicle) {
@@ -104,8 +149,8 @@ public class ResultAssignment {
             }
         }
 
-        System.out.println(String.format("Setting up %s - User: %s", visit, visit.getUserInfo()));
-        printCurrentStatus();
+        //System.out.println(String.format("Setting up %s - User: %s", visit, visit.getUserInfo()));
+        //printCurrentStatus();
     }
 
     private void printCurrentStatus() {
@@ -176,5 +221,27 @@ public class ResultAssignment {
         }
 
         return true;
+    }
+
+    public void showSecondTierAssignedUsers() {
+        List<User> listAssigned = getSortedListOfAssignedUsers();
+        for (User user : listAssigned) {
+            if (!user.isFirstTier())
+                System.out.println(user.getCurrentAssigmentInfo());
+        }
+    }
+
+    public void showFirstTierFromAssigned() {
+        List<User> listAssigned = getSortedListOfAssignedUsers();
+        for (User user : listAssigned) {
+            if (user.isFirstTier())
+                System.out.println(user.getCurrentAssigmentInfo());
+        }
+    }
+
+    private List<User> getSortedListOfAssignedUsers() {
+        List<User> listAssigned = new ArrayList<>(getRequestsOK());
+        listAssigned.sort(Comparator.comparing(User::getPerformanceClass).thenComparing(User::getReqTime));
+        return listAssigned;
     }
 }
