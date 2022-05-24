cd01d0f40a7a5938b8e4a833bc99a08d7910ceab
Breno <brenobeirigo@gmail.com>
2022-05-24 14:49:29 +0200

    Displace and stop

---
+++ src/main/java/model/VisitDisplaceAndStop.java
@@ -0,0 +1,9 @@
+package model;
+
+import model.node.Node;
+
+public class VisitDisplaceAndStop extends VisitRelocation {
+    public VisitDisplaceAndStop(Node middleNode, Vehicle vehicle) {
+        super(middleNode, vehicle);
+    }
+}
